99e3ee7f90400f6af355fb3d60b3c92c13c265f5
Max Nikitin <teachandtravelcn@gmail.com>
2022-02-23 10:20:41 -0600

    changing metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,13 +1,13 @@
 module.exports = {
   pathPrefix: "/gatsby-starter-level-2",
   siteMetadata: {
-    title: "Level 2",
+    title: "maxnikitin.dev",
     titleTemplate: "%s · Level 2",
     image: "/images/logo.png",
     author: "Max Nikitin",
     description:
       "A minimalistic get to know me website build with Gatsby.js",
-    url: "https://knochenmark.github.io",
+    url: "www.maxnikitin.dev",
     paginationPageSize: 4, // Amount of posts displayed per listing page.
   },
   plugins: [
